4bcc146ba16252c26e0a5a1d130c664cf9d8c635
Sunita Nhemafuki <snhemafu@purdue.edu>
2021-07-25 16:41:32 -0500

    IBOOutlet

--- Charts-Tutorial/Charts-Tutorial/ViewController.swift
+++ Charts-Tutorial/Charts-Tutorial/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayBox: UILabel!
+    
+    let quote1 = "Hello World"
+    let quote2 = "Hola Mundo"
+    let quote3 = "Bonjour le monde"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonController(_ sender: Any) {
+        
+        let n = Int.random(in: 1...3)
+        
+        if n == 1
+        {
+            displayBox.text = quote1;
+        }
+        if n == 2
+        {
+            displayBox.text = quote2;
+        }
+        if n == 3
+        {
+            displayBox.text = quote3;
+        }
+    }
 }
+    
+
 
